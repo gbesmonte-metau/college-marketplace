f6960d70faef2d920adea158d2c50b3a6ae038c6
Gianna Ysabel Capino Besmonte <gbesmont@usc.edu>
2025-06-23 14:59:32 -0700

    setup

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my app!')
+})
